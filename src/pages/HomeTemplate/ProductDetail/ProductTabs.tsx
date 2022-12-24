3b5281a47daddab592ac74b0c05ab96f8d106bad
ngocxxu <ngocquach43@gmail.com>
2022-12-24 09:34:00 +0700

    handle submit comment

--- src/pages/HomeTemplate/ProductDetail/ProductTabs.tsx
+++ src/pages/HomeTemplate/ProductDetail/ProductTabs.tsx
@@ -15,24 +15,143 @@ import {
   Typography,
 } from '@mui/material';
 import dayjs from 'dayjs';
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { RootState } from 'src/redux/configStore';
+import { ADD_TO_COMMENT_SAGA, GET_COMMENT_SAGA } from 'src/redux/consts/consts';
+import { IComment } from 'src/types/GeneralTypes';
 import Insta10 from '../../../assets/img/lib/instagram10.jpg';
 import Insta9 from '../../../assets/img/lib/instagram9.jpg';
 import Prod from '../../../assets/img/product/14.1.jpg';
 
+const ListComment = ({ dataComment }: { dataComment: IComment[] }) => {
+  return (
+    <>
+      <Typography
+        sx={{
+          letterSpacing: '3px',
+          position: 'relative',
+          paddingBottom: '10px',
+        }}
+        variant='button'
+        display='block'
+        gutterBottom
+      >
+        CUSTOMER REVIEWS
+        <Box
+          sx={{
+            position: 'absolute',
+            background: '#ff871d',
+            width: '40px',
+            height: '2px',
+            bottom: 0,
+            left: 0,
+          }}
+        ></Box>
+      </Typography>
+
+      {/* ------------- Comments -------------*/}
+      {dataComment.length > 0 ? (
+        dataComment.map((item, index) => {
+          const { content, rate, userId, createdAt } = item;
+          return (
+            <Box
+              key={index}
+              sx={{
+                marginBottom: '30px',
+                marginTop: '20px',
+                border: '1px solid #c4c4c4',
+                borderRadius: '10px',
+                padding: '15px',
+              }}
+            >
+              <Stack
+                justifyContent='space-between'
+                direction='row'
+                alignItems='center'
+              >
+                <Stack
+                  direction='row'
+                  justifyContent='center'
+                  alignItems='center'
+                >
+                  <Avatar
+                    alt={userId.email}
+                    src={`https://avatars.dicebear.com/api/bottts/${userId._id}.svg`}
+                    sx={{ width: 56, height: 56 }}
+                  />
+                  <Box sx={{ marginLeft: '20px' }}>
+                    <Typography variant='subtitle1'>{userId.email}</Typography>
+                    <Typography variant='subtitle2' sx={{ color: '#979797' }}>
+                      {dayjs(createdAt).format('MMMM D, YYYY h:mm A')}
+                    </Typography>
+                  </Box>
+                </Stack>
+                <Box>
+                  <Rating name='read-only' value={rate} readOnly />
+                </Box>
+              </Stack>
+              <Typography
+                sx={{ marginTop: '15px', marginLeft: '15px' }}
+                variant='subtitle1'
+              >
+                {content}
+              </Typography>
+            </Box>
+          );
+        })
+      ) : (
+        <Typography
+          sx={{
+            marginBottom: '30px',
+            marginTop: '20px',
+          }}
+          variant='subtitle2'
+        >
+          No reviews yet
+        </Typography>
+      )}
+    </>
+  );
+};
+
 export const ProductTabs = () => {
+  const formRef = useRef<HTMLFormElement>(null)
+  const { id } = useParams();
+  const dispatch = useDispatch();
   const [value, setValue] = useState('1');
   const [reviewValue, setReviewValue] = useState<number | null>(5);
   const [text, setText] = useState<string | null>();
   const { dataComment } = useSelector(
     (state: RootState) => state.commentReducer
   );
+  const { myInfo } = useSelector((state: RootState) => state.userReducer);
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const handleSubmit = () => {
+    dispatch({
+      type: ADD_TO_COMMENT_SAGA,
+      payload: {
+        id: myInfo._id,
+        idProduct: id,
+        content: text,
+        rate: reviewValue,
+      },
+    });
+
+    console.dir(formRef.current)
+  };
+
+  useEffect(() => {
+    dispatch({
+      type: GET_COMMENT_SAGA,
+      payload: id,
+    });
+  }, [dispatch, id]);
 
   return (
     <Box
@@ -54,7 +173,7 @@ export const ProductTabs = () => {
           >
             <Tab label='DESCRIPTION' value='1' />
             <Tab label='ADDITIONAL INFORMATION' value='2' />
-            <Tab label='REVIEW (5)' value='3' />
+            <Tab label={`REVIEW (${dataComment.length})`} value='3' />
           </TabList>
         </Box>
         <TabPanel value='1'>
@@ -239,95 +358,10 @@ export const ProductTabs = () => {
         </TabPanel>
         <TabPanel value='3'>
           <Container maxWidth='lg'>
-            <Typography
-              sx={{
-                letterSpacing: '3px',
-                position: 'relative',
-                paddingBottom: '10px',
-              }}
-              variant='button'
-              display='block'
-              gutterBottom
-            >
-              CUSTOMER REVIEWS
-              <Box
-                sx={{
-                  position: 'absolute',
-                  background: '#ff871d',
-                  width: '40px',
-                  height: '2px',
-                  bottom: 0,
-                  left: 0,
-                }}
-              ></Box>
-            </Typography>
-            {/* <Typography
-            sx={{
-                marginBottom: '30px',
-                marginTop: '20px',
-              }}
-            variant='subtitle2'
-          >
-            No reviews yet
-          </Typography> */}
-
-            {/* ------------- Comments ------------- */}
-            {dataComment.map((item, index) => {
-              const { content, rate, userId, createdAt } = item;
-              return (
-                <Box
-                  key={index}
-                  sx={{
-                    marginBottom: '30px',
-                    marginTop: '20px',
-                    border: '1px solid #c4c4c4',
-                    borderRadius: '10px',
-                    padding: '15px',
-                  }}
-                >
-                  <Stack
-                    justifyContent='space-between'
-                    direction='row'
-                    alignItems='center'
-                  >
-                    <Stack
-                      direction='row'
-                      justifyContent='center'
-                      alignItems='center'
-                    >
-                      <Avatar
-                        alt={userId.email}
-                        src={`https://avatars.dicebear.com/api/bottts/${userId._id}.svg`}
-                        sx={{ width: 56, height: 56 }}
-                      />
-                      <Box sx={{ marginLeft: '20px' }}>
-                        <Typography variant='subtitle1'>
-                          {userId.email}
-                        </Typography>
-                        <Typography
-                          variant='subtitle2'
-                          sx={{ color: '#979797' }}
-                        >
-                          {dayjs(createdAt).format('MMMM D, YYYY h:mm A')}
-                        </Typography>
-                      </Box>
-                    </Stack>
-                    <Box>
-                      <Rating name='read-only' value={rate} readOnly />
-                    </Box>
-                  </Stack>
-                  <Typography
-                    sx={{ marginTop: '15px', marginLeft: '15px' }}
-                    variant='subtitle1'
-                  >
-                    {content}
-                  </Typography>
-                </Box>
-              );
-            })}
+            <ListComment dataComment={dataComment} />
 
             <Divider />
-            
+
             <Stack direction='row' spacing={2} alignItems='center'>
               <Typography
                 sx={{
@@ -357,10 +391,11 @@ export const ProductTabs = () => {
               sx={{ marginTop: '15px' }}
             >
               <Box
+                ref={formRef}
                 sx={{ width: '100%' }}
                 component='form'
                 onSubmit={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  console.log('submit called');
+                  handleSubmit();
                   e.preventDefault();
                 }}
               >
