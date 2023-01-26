f925d1446a087387601f9a48536aff7f4199a63c
ngocxxu <ngocquach43@gmail.com>
2023-01-26 23:44:08 +0700

    commit yml

--- src/pages/HomeTemplate/ProductDetail/index.tsx
+++ src/pages/HomeTemplate/ProductDetail/index.tsx
@@ -98,6 +98,7 @@ const RelatedProducts = () => {
       >
         RELATED PRODUCTS
       </Typography>
+      
       {!isPendingAllProduct && dataAllProducts.length > 0 ? (
         <RelatedProductSwiper />
       ) : (
