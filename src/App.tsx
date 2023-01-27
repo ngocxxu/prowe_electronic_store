51c394e72197bc45d9d61c0de94c2ba9c4454c38
ngocxxu <ngocquach43@gmail.com>
2023-01-27 23:15:43 +0700

    yml15

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import Register from './pages/UserTemplate/Register';
 import RegisterSuccess from './pages/UserTemplate/Register/RegisterSuccess';
 
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from 'firebase/analytics';
+// import { getAnalytics } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
 import { lazy, Suspense } from 'react';
 import { LoadingPage } from './components/Loading';
@@ -37,8 +37,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const HomeTemplateLazy = lazy(() => import('./pages/HomeTemplate'));
 
