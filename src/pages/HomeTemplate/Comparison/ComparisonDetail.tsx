de72eb32e31f8c115d818eb8bc5394034d559650
ngocxxu <ngocquach43@gmail.com>
2023-02-04 23:59:07 +0700

    render UI for product comparison

--- src/pages/HomeTemplate/Comparison/ComparisonDetail.tsx
+++ src/pages/HomeTemplate/Comparison/ComparisonDetail.tsx
@@ -23,15 +23,35 @@ const ComparisonDetail = () => {
       >
         <TableHead>
           <TableRow>
-            <TableCell align='center'>Settings</TableCell>
-            <TableCell>Ultra Android 10.0 Smartphone 8GB Ram</TableCell>
+            <TableCell className='w-10' align='center'>
+              Settings
+            </TableCell>
+            <TableCell className='w-20'>
+              Ultra Android 10.0 Smartphone 8GB Ram
+            </TableCell>
+            <TableCell className='w-20'>
+              Ultra Android 10.0 Smartphone 8GB Ram a Android 10.0 Smartphone 8
+            </TableCell>
+            <TableCell className='w-20'>
+              Ultra Android 10.0 Smartphone 8GB Ram
+            </TableCell>
+            <TableCell className='w-20'>
+              Ultra Android 10.0 Smartphone 8GB Ram
+            </TableCell>
+            <TableCell className='w-20'>
+              Ultra Android 10.0 Smartphone 8GB Ram
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center'>Image</TableCell>
+            <TableCell align='center'>
+              <Typography variant='subtitle2' gutterBottom>
+                Image
+              </Typography>
+            </TableCell>
             <TableCell>
-              <div>
+              <div className='max-w-[170px]'>
                 <img
                   className='max-w-full rounded-sm'
                   src={Prod1}
@@ -41,40 +61,68 @@ const ComparisonDetail = () => {
             </TableCell>
           </TableRow>
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center'>Price</TableCell>
+            <TableCell align='center'>
+              <Typography variant='subtitle2' gutterBottom>
+                Price
+              </Typography>
+            </TableCell>
             <TableCell>
-              <Typography variant='button' display='block' gutterBottom>
+              <Typography variant='h6' display='block' gutterBottom>
                 $54.50
               </Typography>
             </TableCell>
           </TableRow>
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center'>Description</TableCell>
-            <TableCell className='w-64'>
-              <div>
+            <TableCell align='center'>
+              <Typography variant='subtitle2' gutterBottom>
+                Description
+              </Typography>
+            </TableCell>
+            <TableCell>
+              <Typography variant='body1'>
                 At vero eos et accusamus et iusto odio dignissimos ducimus qui
                 blanditiis praesentium voluptatum deleniti atque corrupti quos
                 dolores et quas molestias excepturi sint occaecati cupiditate
                 non provident.
-              </div>
+              </Typography>
             </TableCell>
           </TableRow>
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center'>Dimensions</TableCell>
+            <TableCell align='center'>
+              <Typography variant='subtitle2' gutterBottom>
+                Dimensions
+              </Typography>
+            </TableCell>
             <TableCell>
               <div>N/A</div>
             </TableCell>
           </TableRow>
           <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell align='center'>Add to cart</TableCell>
+            <TableCell align='center'>
+              <Typography variant='subtitle2' gutterBottom>
+                Actions
+              </Typography>
+            </TableCell>
             <TableCell>
               <Button
                 // onClick={() => navigate('/shop')}
                 size='large'
                 variant='contained'
-                sx={{ backgroundColor: 'black' }}
+                sx={{
+                  backgroundColor: 'black',
+                  '&:hover': { backgroundColor: 'black' },
+                  mr: 1,
+                }}
               >
                 Add To Cart
+              </Button>
+              <Button
+                // onClick={() => navigate('/shop')}
+                size='large'
+                variant='contained'
+                color='error'
+              >
+                Remove
               </Button>
             </TableCell>
           </TableRow>
